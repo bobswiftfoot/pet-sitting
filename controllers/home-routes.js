f8d9cc8b9af4f383b177fe075f16e60627a1bfaa
Nate Pfau <natepfau@yahoo.com>
2021-08-02 22:34:27 -0500

    Added html routes to each planned page

---
+++ controllers/home-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+
+router.get('/', (req, res) =>
+{
+    res.render('homepage');
+});
+
+router.get('/login', (req, res) =>
+{
+    res.render('login');
+});
+
+router.get('/profile', (req, res) =>
+{
+    res.render('profile');
+});
+
+router.get('/calendar', (req, res) =>
+{
+    res.render('calendar');
+});
