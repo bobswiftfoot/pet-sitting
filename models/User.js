274fd24fdc3b42bedfb50851974345b7d13ee48e
Samantha Malone <sammac99@gmail.com>
2021-08-02 20:46:29 -0700

    added columns to user model

--- models/User.js
+++ models/User.js
@@ -11,6 +11,35 @@ class User extends Model {
 }
 
 // create fields / columns for User models
+User.init(
+  {
+    id: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_name: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freeseTableName: true,
+    underscored: true,
+    modelName: "user",
+  }
+);
 // set up beforeCreate lifecycle "hook" functionality
 // setup beforeUpdate lifecycle "hook" functionality
 // export User
