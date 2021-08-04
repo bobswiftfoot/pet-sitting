2d24b5ee2a6ee76bc01a2494a9b73c3173193fd3
Nate Pfau <natepfau@yahoo.com>
2021-08-03 19:16:17 -0500

    Added careday routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const petRoutes = require('./pet-routes');
+const caredayRoutes = require('./careday-routes');
 
 router.use('/users', userRoutes);
 router.use('/pets', petRoutes); 
+router.use('/caredays', caredayRoutes); 
 
 module.exports = router;
