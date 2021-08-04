563f0db76a2676382b6c0b05147ede445d602104
Samantha Malone <sammac99@gmail.com>
2021-08-04 11:29:41 -0700

    added Pets model

--- models/Pet.js
+++ models/Pet.js
@@ -2,6 +2,36 @@ const { Model, DataTypes } = require("sequelize");
 const Sequelize = require("../config/connection");
 
 // Create Pet model
+class Pets extends Model {}
+
 // create fields/columns for Pet models
+Pets.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    pet_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "pet",
+  }
+);
+
 // export Pet model
 module.exports = Pet;
