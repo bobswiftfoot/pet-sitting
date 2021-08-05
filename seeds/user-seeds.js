a13b633a1bd51249fe87d797a3189e9ea7d1d215
Samantha Malone <sammac99@gmail.com>
2021-08-04 20:01:43 -0700

    added user seeds

---
+++ seeds/user-seeds.js
@@ -0,0 +1,30 @@
+const sequelize = require("../config/connection");
+const { User, Pet, CareDay } = require("../models");
+
+const userData = [
+  {
+    username: "rainsample2",
+    email: "rsample2@gmail.com",
+    password: "password123",
+  },
+  {
+    username: "onelove43",
+    email: "onelove@gmail.com",
+    password: "youcantguessthis",
+  },
+  {
+    username: "fireandice24",
+    email: "brr1212@gmail.com",
+    password: "hotandcold",
+  },
+  {
+    username: "anotherexample14",
+    email: "ae14@gmail.com",
+    password: "freezedance2!8",
+  },
+  {
+    username: "incredible980",
+    email: "hulk980@gmail.com",
+    password: "superhuman01",
+  },
+];
