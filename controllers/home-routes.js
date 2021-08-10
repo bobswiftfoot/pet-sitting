0f751432401d754822c4026197af9d70d6b15674
Samantha Malone <sammac99@gmail.com>
2021-08-09 19:42:07 -0700

    fixed posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,157 +1,151 @@
-const router = require('express').Router();
-const { User, Pet, CareDay, Post, Comment } = require('../models');
-const sequelize = require('../config/connection');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, Pet, CareDay, Post, Comment } = require("../models");
+const sequelize = require("../config/connection");
+const withAuth = require("../utils/auth");
 
-router.get('/', (req, res) =>
-{
-    res.render('homepage',{ loggedIn: req.session.loggedIn });
+router.get("/", (req, res) => {
+  res.render("homepage", { loggedIn: req.session.loggedIn });
 });
 
-router.get('/login', (req, res) =>
-{
-    if (req.session.loggedIn)
-    {
-        res.redirect('/');
-        return;
-    }
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
 
-    res.render('login',{ loggedIn: req.session.loggedIn });
+  res.render("login", { loggedIn: req.session.loggedIn });
 });
 
-router.get('/profile', withAuth, (req, res) =>
-{   
-    User.findAll({
-        where: {
-            id: req.session.user_id
-        }, 
-        attributes: { exclude: ['password'] },
+router.get("/profile", withAuth, (req, res) => {
+  User.findAll({
+    where: {
+      id: req.session.user_id,
+    },
+    attributes: { exclude: ["password"] },
+    include: [
+      {
+        model: Pet,
+        as: "pets",
+        attributes: [
+          "id",
+          "pet_name",
+          "pet_animal",
+          "pet_breed",
+          "picture_file_name",
+        ],
         include: [
-            {
-                model: Pet,
-                as: 'pets',
-                attributes: ['id', 'pet_name', 'pet_animal', 'pet_breed', 'picture_file_name'],
-                include: [
-                    {
-                        model: CareDay,
-                        as: 'requested_care_days',
-                        attributes: ['id']
-                    }
-                ]
-            },
-            {
-                model: CareDay,
-                as: 'sitting_days',
-                attributes: ['id']
-            }
-        ]
+          {
+            model: CareDay,
+            as: "requested_care_days",
+            attributes: ["id"],
+          },
+        ],
+      },
+      {
+        model: CareDay,
+        as: "sitting_days",
+        attributes: ["id"],
+      },
+    ],
+  })
+    .then((dbUserData) => {
+      const user = dbUserData.map((user) => user.get({ plain: true }))[0];
+      res.render("profile", { user, loggedIn: req.session.loggedIn });
     })
-    .then(dbUserData =>
-    {
-        const user = dbUserData.map(user => user.get({ plain: true }))[0];
-        res.render('profile', { user, loggedIn: req.session.loggedIn });
-    })
-    .catch(err =>
-    {
-        console.log(err);
-        res.status(500).json(err);
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
-router.get('/calendar', withAuth, (req, res) =>
-{
-    CareDay.findAll({
-        order:[['day_of_care', 'ASC']],
+router.get("/calendar", withAuth, (req, res) => {
+  CareDay.findAll({
+    order: [["day_of_care", "ASC"]],
+    include: [
+      {
+        model: Pet,
+        as: "requested_care_days",
+        attributes: ["id", "pet_name"],
+        include: {
+          model: User,
+          as: "owner",
+          attributes: ["id", "user_name", "email"],
+        },
+      },
+      {
+        model: User,
+        as: "sitting_days",
+        attributes: ["id", "user_name", "email"],
+      },
+    ],
+  })
+    .then((dbCareDayData) => {
+      User.findAll({
+        where: {
+          id: req.session.user_id,
+        },
+        attributes: { exclude: ["password"] },
         include: [
-            {
-                model: Pet,
-                as: 'requested_care_days',
-                attributes: ['id', 'pet_name'],
-                include:
-                {
-                    model: User,
-                    as: 'owner',
-                    attributes: ['id', 'user_name', 'email']
-                }
-            },
-            {
-                model: User,
-                as: 'sitting_days',
-                attributes: ['id', 'user_name', 'email']
-            }
-        ]
+          {
+            model: Pet,
+            as: "pets",
+            attributes: ["id", "pet_name", "pet_animal", "pet_breed"],
+          },
+        ],
+      }).then((dbUserData) => {
+        const user = dbUserData.map((user) => user.get({ plain: true }))[0];
+
+        const caredays = dbCareDayData.map((careday) =>
+          careday.get({ plain: true })
+        );
+        res.render("calendar", {
+          caredays,
+          user,
+          loggedIn: req.session.loggedIn,
+        });
+      });
     })
-        .then(dbCareDayData => 
-        {
-            User.findAll({
-                where: {
-                    id: req.session.user_id
-                }, 
-                attributes: { exclude: ['password'] },
-                include: [
-                    {
-                        model: Pet,
-                        as: 'pets',
-                        attributes: ['id', 'pet_name', 'pet_animal', 'pet_breed'],
-                    }
-                ]
-            })
-            .then(dbUserData =>
-            {
-                const user = dbUserData.map(user => user.get({ plain: true }))[0];
-
-                const caredays = dbCareDayData.map(careday => careday.get({ plain: true }));
-                res.render('calendar' ,{ caredays, user, loggedIn: req.session.loggedIn });
-            })
-        })
-        .catch(err =>
-        {
-            console.log(err);
-            res.status(500).json(err);
-        });
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.get('/edit-post', withAuth, (req, res) =>
-{
-    res.render('edit-post' ,{ loggedIn: req.session.loggedIn });
+router.get("/edit-post", withAuth, (req, res) => {
+  res.render("edit-post", { loggedIn: req.session.loggedIn });
 });
 
-router.get('/single-post', withAuth, (req, res) =>
-{
-    res.render('single-post' ,{ loggedIn: req.session.loggedIn });
+router.get("/single-post", withAuth, (req, res) => {
+  res.render("single-post", { loggedIn: req.session.loggedIn });
 });
 
-router.get('/posts', withAuth, (req, res) =>
-{
-    Post.findAll(
-        {
-            include: [
-                {
-                    model: Comment,
-                    attributes: ['comment_text', 'created_at'],
-                    include: [
-                        {
-                            model: User,
-                            attributes: ['user_name']
-                        }
-                    ]
-                },
-                {
-                    model: User,
-                    attributes: ['user_name']
-                }
-            ]
-        })
-        .then(dbPostData => 
-        {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('posts' ,{ posts, loggedIn: req.session.loggedIn });
-        })
-        .catch(err =>
-        {
-            console.log(err);
-            res.status(500).json(err);
-        });
+router.get("/posts", withAuth, (req, res) => {
+  Post.findAll({
+    include: [
+      {
+        model: Comment,
+        attributes: ["comment_text", "createdAt"],
+        include: [
+          {
+            model: User,
+            attributes: ["user_name"],
+          },
+        ],
+      },
+      {
+        model: User,
+        attributes: ["user_name"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("posts", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,20 +1,30 @@
 module.exports = {
-    //For debuging values sent to handlebars
-    debug: optionalValue =>
-    {
-        console.log("Current Context");
-        console.log("====================");
-        console.log(this);
-    
-        if (optionalValue) {
-        console.log("Value");
-        console.log("====================");
-        console.log(optionalValue);
-        }
-    },
-    formatDate: date=>
-    {
-        let dateFormat = date.split("-");
-        return dateFormat[1] + "/" + dateFormat[2] + "/" + dateFormat[0];
+  //For debuging values sent to handlebars
+  debug: (optionalValue) => {
+    console.log("Current Context");
+    console.log("====================");
+    console.log(this);
+
+    if (optionalValue) {
+      console.log("Value");
+      console.log("====================");
+      console.log(optionalValue);
     }
-};  
+  },
+  formatDate: (date) => {
+    let dateFormat = date.split("-");
+    return dateFormat[1] + "/" + dateFormat[2] + "/" + dateFormat[0];
+  },
+  // adding format date helper
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+    return word;
+  },
+};
